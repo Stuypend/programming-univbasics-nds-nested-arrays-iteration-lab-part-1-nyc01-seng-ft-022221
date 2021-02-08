a1e8100a926b44c632c68c2edf9c6311396d33e9
Andre Powers <andre@stuypend.com>
2021-02-08 16:02:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,9 @@ def find_even_values(src)
         {
           puts src[row][col]
         }
+        col += 1
     end
+    row += 1
   end
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
